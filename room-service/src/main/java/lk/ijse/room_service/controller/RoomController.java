16ee3d2b89806bbc4944b320b9031eab374ecf14
dinildamsith <dinildamsith52@gmail.com>
2024-08-04 11:11:58 +0530

    room save done

--- room-service/src/main/java/lk/ijse/room_service/controller/RoomController.java
+++ room-service/src/main/java/lk/ijse/room_service/controller/RoomController.java
@@ -1,15 +1,44 @@
 package lk.ijse.room_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.room_service.dto.RoomDTO;
+import lk.ijse.room_service.enums.RoomType;
+import lk.ijse.room_service.service.RoomServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/room_service")
 public class RoomController {
 
+    @Autowired
+    RoomServices roomServices;
+
     @GetMapping
     String checkHealth(){
         return "Worked...";
+    }
+
+
+    @RequestMapping("/save")
+    String saveRoom(@RequestPart("roomId") String roomId,
+                    @RequestPart("roomPic") String roomPic,
+                    @RequestPart("roomType") String roomType,
+                    @RequestPart("beadCount") String beadCount,
+                    @RequestPart("oneDayPrice") String oneDayPrice
+                               ){
+
+        RoomDTO roomDTO = new RoomDTO();
+        roomDTO.setRoomId(Integer.parseInt(roomId));
+        roomDTO.setRoomPic(roomPic);
+        roomDTO.setRoomType(RoomType.valueOf(roomType));
+        roomDTO.setBeadCount(Integer.parseInt(beadCount));
+        roomDTO.setOneDayPrice(Double.parseDouble(oneDayPrice));
+
+
+        String res = roomServices.saveRooms(roomDTO);
+        return res;
     }
 }
--- room-service/src/main/java/lk/ijse/room_service/convert/DataConvert.java
+++ room-service/src/main/java/lk/ijse/room_service/convert/DataConvert.java
@@ -1,21 +1,23 @@
-//package lk.ijse.room_service.convert;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DataConvert {
-//
-//    private final ModelMapper modelMapper;
-//
-//
-//    public TicketDTO ticketEntityConvertTicketDTO(TicketEntity ticketEntity){
-//        return modelMapper.map(ticketEntity, TicketDTO.class);
-//    }
-//
-//    public TicketEntity ticketDTOConvertTicketEntity(TicketDTO ticketDTO){
-//        return modelMapper.map(ticketDTO, TicketEntity.class);
-//    }
-//}
+package lk.ijse.room_service.convert;
+
+import lk.ijse.room_service.dto.RoomDTO;
+import lk.ijse.room_service.entity.RoomEntity;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataConvert {
+
+    private final ModelMapper modelMapper;
+
+
+    public RoomDTO roomEntityConvertRoomDTO(RoomEntity roomEntity){
+        return modelMapper.map(roomEntity, RoomDTO.class);
+    }
+
+    public RoomEntity roomDTOConvertRoomEntity(RoomDTO roomDTO){
+        return modelMapper.map(roomDTO, RoomEntity.class);
+    }
+}
