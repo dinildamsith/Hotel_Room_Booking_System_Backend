c13808982d940b537e9b5e289cfec7202e119147
dinildamsith <dinildamsith52@gmail.com>
2024-08-04 09:28:12 +0530

    config server added

---
+++ config-sever/src/main/java/lk/ijse/config_sever/ConfigSeverApplication.java
@@ -0,0 +1,17 @@
+package lk.ijse.config_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSeverApplication.class, args);
+	}
+
+}
---
+++ config-sever/src/test/java/lk/ijse/config_sever/ConfigSeverApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.config_sever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSeverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
